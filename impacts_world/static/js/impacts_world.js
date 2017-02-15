6ee56a1142a875504913b0aeb49be5b3c37f62e8
Matthias Brück <hi@brueck.io>
2017-02-15 10:20:45 +0100

    styling

--- impacts_world/static/js/impacts_world.js
+++ impacts_world/static/js/impacts_world.js
@@ -1,17 +1,17 @@
-// $(function() {
-//   $('a.anchor-scroll').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 800);
-//         return false;
-//       }
-//     }
-//   });
-// });
+$(function() {
+  $('a.anchor-scroll').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
 
 $(document).ready(function () {
 
