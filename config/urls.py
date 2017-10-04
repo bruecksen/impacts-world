c9e712f07c1c421b2977cad9e5e6a7cc06db1feb
Matthias Brück <hi@brueck.io>
2017-10-04 12:18:10 +0200

    add wagtail documents url

--- config/urls.py
+++ config/urls.py
@@ -8,11 +8,13 @@ from django.views import defaults as default_views
 
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^cms/', include(wagtailadmin_urls)),
+    url(r'^documents/', include(wagtaildocs_urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
